9f8c359989d5d3c0e978a18b14ad210949891126
fabi0jr <faelfjr35@gmail.com>
2025-04-19 13:43:08 -0400

    Add Banner component with styling for homepage banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Banner from "./components/Banner";
 
 
 function App() {
   return (
     <>
     <Header />
+    <Banner />
     <h1>Hello World!</h1>
     <p>Div pai e os caralho</p>
     <Footer />
