11c2c722089b1601d7dd20885084a64b11432923
fabi0jr <faelfjr35@gmail.com>
2025-04-26 11:35:39 -0400

    Adicionar roteamento com react-router-dom e estruturar páginas Home e Watch

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
-
 
 function Header(){
     return(
         
         <header className={styles.header}>
-            <span>FabioFlix</span>
+                <Link to="/">
+                    <span>FabioFlix</span>
+                </Link>
             <nav>
-                <a href="#">Home</a>
-                <a href="#">Assistir</a>
+                <Link to="/">Home</Link>
+                <Link to="/watch">Watch</Link>
             </nav>
         </header>   
      )
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Watch from "./pages/Watch";
+
+function AppRoutes(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Home /> } />
+                <Route path="/watch" element={ <Watch /> }/>
+            </Routes>
+        </BrowserRouter>
+
+
+    )
+}
+
+export default AppRoutes;
